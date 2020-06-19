e90c2645b05b0012b9433ad28ce4363706c9a95b
lewismcruz <lewis.cruz@gmail.com>
2020-06-19 11:32:03 -0500

    Schema update merging to lewis branch

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,71 @@
-DROP DATABASE IF EXISTS fitnessDB;
+### Schema
 
-CREATE DATABASE fitnessDB;
+CREATE DATABASE funactiv_db;
+USE funactiv_db;
 
-USE fitnessDB;
+-- I used 'amember' for 'user' - not married  to it.  Member work is a reserved MySQL keyword. If team wants we can change to 'user', we can.
 
--- Create the table plans.
-CREATE TABLE users (
-  id int NOT NULL AUTO_INCREMENT,
-  email varchar(255) NOT NULL,
-  pass varchar(255) NOT NULL,
-  PRIMARY KEY (id)
+-- Missing datatypes for a few metrics.  Need to confirm are these 'time' values and how many decimal places do we want to hold.
+
+CREATE TABLE amember
+(
+    id INT(11) AUTO_INCREMENT NOT NULL,
+	amember_id INT(11),
+	first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    weight FLOAT(5,2) NOT NULL,
+    height FLOAT(2,1) NOT NULL,
+    gender CHAR,
+    active_mins INT(2),
+    lifetime_total INT,
+    carb_intake INT,
+    protien_intake INT,
+    fat_intake INT,
+	PRIMARY KEY (amember_id)
+    
+);
+
+CREATE TABLE friends_list
+(
+    friend_id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(20) NOT NULL,
+    last_name VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    gender VARCHAR(1) NOT NULL,
+    amember_refer_id int NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (friend_id),
+    FOREIGN KEY (amember_id) REFERENCES amember(id)
+);
+
+CREATE TABLE blog 
+(
+   entry_id INT NOT NULL AUTO_INCREMENT,
+   blogpost_title VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE goals
+(
+   goal_id INT NOT NULL AUTO_INCREMENT,                                                                               
+   category VARCHAR(50) NOT NULL,
+   carbs_goal INT NOT NULL,
+   protein_goal INT NOT NULL,
+   fat_goal INT NOT NULL,
+   calories INT NOT NULL,
+   active_mins INT NOT NULL,
+   activity_date DATE,
+   time_stamp TIMESTAMP(6) 
+);
+
+CREATE TABLE challenge
+(
+   id INT(11) AUTO_INCREMENT NOT NULL,
+   title VARCHAR(30) NOT NULL,
+   challenge_type VARCHAR(10) NOT NULL,
+   challenge_extend BOOLEAN,
+   challenge_status VARCHAR(10),
+   amember_id INT(11),
+   PRIMARY KEY (id),
+   FOREIGN KEY (friend_id) REFERENCES friend(id)
+
 );
