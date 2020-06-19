e2af298f388e75c7a82089087e19a0cd8a505a17
Celeste J Bembry <CelesteJBembry@gmail.com>
2020-06-19 10:15:24 -0500

    project work from Thursday evening. did some work on the database for height and weight. initial layout for nutrition page with chart

--- Sequelize-Passport-Example/schema.sql
+++ Sequelize-Passport-Example/schema.sql
@@ -9,5 +9,45 @@ CREATE TABLE users (
   id int NOT NULL AUTO_INCREMENT,
   email varchar(255) NOT NULL,
   pass varchar(255) NOT NULL,
+  female BOOLEAN, NOT NULL
+  weight int,
+  height int,
+  -- https://stackoverflow.com/questions/6591613/saving-users-height-and-weight
+  calories int,
+  fat int,
+  protein int,
+  carbs int,
   PRIMARY KEY (id)
 );
+INSERT INTO users (movie) VALUES ('Rush Hour 2');
+
+
+
+
+-- formatting height into feet'inches" format
+
+CREATE TABLE height (
+    id          SERIAL PRIMARY KEY,
+    english     VARCHAR,
+    inches      INT,
+    cm          INT,
+    hands       INT  // As in, the height of a horse
+);
+INSERT INTO height VALUES
+    (1,'4 feet',           48, 122, 12),
+    (2,'4 feet, 1 inch',   49, 124, 12),
+    (3,'4 feet, 2 inches', 50, 127, 12),
+    (3,'4 feet, 3 inches', 51, 130, 12),
+
+    CREATE TABLE users (
+    uid         SERIAL PRIMARY KEY,
+    height      INT REFERENCES height(id),
+    weight      INT references weight(id),
+    sign        INT references sign(id),
+    ...
+);
+
+SELECT *
+FROM users
+JOIN height ON users.height = height.id
+WHERE height.inches >= 48 AND height.inches <= 60;
