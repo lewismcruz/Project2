b459e79016a484401939dc4e68b39415f744055a
Celeste J Bembry <CelesteJBembry@gmail.com>
2020-06-22 11:42:53 -0500

    updates for bar chart

--- public/js/barChart.js
+++ public/js/barChart.js
@@ -1,7 +1,7 @@
-const ctx = document.getElementById("myChart");
+const ctx = document.getElementById("fitnessChart");
 
-const age = [20, 19, 25, 23];
-const users = ["Celeste", "Lewis", "Brandon", "Joel"];
+const age = [30, 0, 50, 35, 73, 0, 0];
+const users = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 // eslint-disable-next-line no-unused-vars
 const myChart = new Chart(ctx, {
@@ -10,7 +10,32 @@ const myChart = new Chart(ctx, {
     labels: users,
     datasets: [
       {
-        label: "Team",
+        label: "Week 1",
+        data: age,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)"
+        ]
+      }
+    ]
+  }
+});
+
+const ctx = document.getElementById("nutritionChart");
+
+const age = [30, 0, 50, 35, 73, 0, 0];
+const users = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// eslint-disable-next-line no-unused-vars
+const myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: users,
+    datasets: [
+      {
+        label: "Activity Minutes",
         data: age,
         backgroundColor: [
           "rgba(255, 99, 132, 0.2)",
