7e9bfd8b3f140d8dacadb8b75ae4834a654b8862
gatekeeper <63681296+truittj@users.noreply.github.com>
2020-06-22 17:21:47 -0500

    saving before i pull from lewis

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,37 +1,25 @@
-// const moment = require('moment'); // require
-// moment().format();
-// Requiring path to so we can use relative routes to our HTML files
-//const path = require("path");
-
-// Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 const db = require("../models");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
-    // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("members");
     }
-    res.render("../views/login");
+    res.render("login");
   });
 
   app.get("/login", (req, res) => {
-    // If the user already has an account send them to the members page
-    console.log(req + " hello");
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("members");
     }
-    res.render("../views/login");
+    res.render("login");
   });
 
   app.get("/signup", (req, res) => {
-    // If the user already has an account send them to the members page
-    res.render("../views/signup");
+    res.render("signup");
   });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
     db.User.findOne({
       where: {
@@ -41,9 +29,6 @@ module.exports = function(app) {
       const timeEl = JSON.stringify(user.dataValues.createdAt);
       const newTime = timeEl.split("-");
       const sliceTime = newTime[0].slice(1);
-      // console.log(timeEl);
-      // console.log(newTime);
-      // console.log(sliceTime);
 
       const userObj = {
         firstName: user.dataValues.firstName,
@@ -54,25 +39,7 @@ module.exports = function(app) {
       res.render("membersDashboard", userObj);
     });
   });
-
-  //   app.get("/test/:id", (req, res) => {
-  //     db.User.findOne({
-  //       where: {
-  //         id: req.params.id
-  //       }
-  //     }).then(user => {
-  //       // const user = [];
-
-  //       // for (let i = 0; i < dbUser.length; i++) {
-  //       //   const user1 = {};
-  //       //   (user1.firstName = dbUser[i].firstName);
-  //       //     (user1.lastName = dbUser[i].lastName);
-  //       //     (user1.createdAt = dbUser[i].createdAt);
-  //       //   user.push(user1);
-  //       // }
-
-  //       // console.log(user);
-  //       res.render("test", user);
-  //     });
-  //   });
+  app.get("/goals", (req, res) => {
+    res.render("goalschoice");
+  });
 };
